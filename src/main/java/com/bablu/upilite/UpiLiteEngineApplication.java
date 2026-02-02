853e158fb9da17397a1bf3e6d2ffd13edc30e944
bablu kumar <bablu707041dk@gmail.com>
2026-02-02 20:51:53 +0530

    Intial commit: Spring Boot 4.0.2 structure with java 21

---
+++ src/main/java/com/bablu/upilite/UpiLiteEngineApplication.java
@@ -0,0 +1,13 @@
+package com.bablu.upilite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiLiteEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpiLiteEngineApplication.class, args);
+	}
+
+}
